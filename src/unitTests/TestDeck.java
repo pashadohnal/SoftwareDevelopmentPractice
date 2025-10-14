34648cef8e676bc4365b25e1ed5ef000d71bd649
LAU Lapheiissac <issaclau2-c@my.cityu.edu.hk>
2025-10-14 15:59:52 +0800

    Refactor test methods and update assertions

--- src/unitTests/TestDeck.java
+++ src/unitTests/TestDeck.java
@@ -22,19 +22,35 @@ public class TestDeck {
     }
     
     @Test
-    public void testCal() {
+    public void testCal1() {
     	ArrayList<Character> hand = new ArrayList<>();
     	hand.add('K');
     	assertEquals(Deck.calValue(hand), 10);
     	hand.add('A');
     	assertEquals(Deck.calValue(hand), 21);
     	hand.add('A');
-    	assertEquals(Deck.calValue(hand), 22);
+    	assertEquals(Deck.calValue(hand), 12);
     	hand.add('Q');
-    	assertEquals(Deck.calValue(hand), 32);
+    	assertEquals(Deck.calValue(hand), "BUST");
     	hand.add('J');
-    	assertEquals(Deck.calValue(hand), 42);
+    	assertEquals(Deck.calValue(hand), "BUST");
     	hand.add('5');
-    	assertEquals(Deck.calValue(hand), 47);
+    	assertEquals(Deck.calValue(hand), "BUST");
+    }
+    @Test
+    public void testCal2() {
+    	ArrayList<Character> hand = new ArrayList<>();
+    	hand.add('K');
+    	assertEquals(Deck.calValue(hand), 10);
+    	hand.add('K');
+    	assertEquals(Deck.calValue(hand), 20);
+    	hand.add('X');
+    	assertEquals(Deck.calValue(hand), 20);
+    	hand.add('A');
+    	assertEquals(Deck.calValue(hand), 21);
+    	hand.add('A');
+    	assertEquals(Deck.calValue(hand), "BUST");
+    	hand.add('5');
+    	assertEquals(Deck.calValue(hand), "BUST");
     }
 }
