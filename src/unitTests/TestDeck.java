e041db2c363f5863587c5bebbe4b7f81f50f8a81
Pavel Dohnal <pavd@itu.dk>
2025-10-17 10:44:54 +0800

    Deleted Tests

--- src/unitTests/TestDeck.java
+++ src/unitTests/TestDeck.java
@@ -20,37 +20,4 @@ public class TestDeck {
     	// check can Deck get second card
     	assertEquals(Deck.draw(), cards.get(1));
     }
-    
-    @Test
-    public void testCal1() {
-    	ArrayList<Character> hand = new ArrayList<>();
-    	hand.add('K');
-    	assertEquals(Deck.calValue(hand), 10);
-    	hand.add('A');
-    	assertEquals(Deck.calValue(hand), 21);
-    	hand.add('A');
-    	assertEquals(Deck.calValue(hand), 12);
-    	hand.add('Q');
-    	assertEquals(Deck.calValue(hand), "BUST");
-    	hand.add('J');
-    	assertEquals(Deck.calValue(hand), "BUST");
-    	hand.add('5');
-    	assertEquals(Deck.calValue(hand), "BUST");
-    }
-    @Test
-    public void testCal2() {
-    	ArrayList<Character> hand = new ArrayList<>();
-    	hand.add('K');
-    	assertEquals(Deck.calValue(hand), 10);
-    	hand.add('K');
-    	assertEquals(Deck.calValue(hand), 20);
-    	hand.add('X');
-    	assertEquals(Deck.calValue(hand), 20);
-    	hand.add('A');
-    	assertEquals(Deck.calValue(hand), 21);
-    	hand.add('A');
-    	assertEquals(Deck.calValue(hand), "BUST");
-    	hand.add('5');
-    	assertEquals(Deck.calValue(hand), "BUST");
-    }
 }
