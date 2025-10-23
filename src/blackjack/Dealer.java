a9e64576ecfb4d27302c8baeab7495d2f894a53c
LAU Lapheiissac <issaclau2-c@my.cityu.edu.hk>
2025-10-23 21:52:32 +0800

    Update Dealer.java

--- src/blackjack/Dealer.java
+++ src/blackjack/Dealer.java
@@ -1,6 +1,23 @@
 package blackjack;
 
 public class Dealer extends Player {
-
+	public Dealer() {
+		super();
+	}
+	public void play() {
+		while (getValue() < 17) {
+			draw();
+		}
+	}
+	public void showHand(boolean showAll) {
+		if(showAll) {
+			System.out.println("Dealer's hand value: " + getValue());
+		} else if(hand.size()>1){
+			Character firstCard = hand.get(0);
+			System.out.println("Dealer's hand value: " + Blackjack.valueOf(0, firstCard) + " + [hidden]");
+		}
+		else {
+			System.out.println("Dealer has no cards yet.");
+		}
+	}
 }
-
