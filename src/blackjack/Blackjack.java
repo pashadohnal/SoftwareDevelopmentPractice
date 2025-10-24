dd8546fd3f0f2aa73e0358f16c798441efb20f5b
Pavel Dohnal <74378788+pashadohnal@users.noreply.github.com>
2025-10-24 10:50:47 +0800

    Update Blackjack.java

--- src/blackjack/Blackjack.java
+++ src/blackjack/Blackjack.java
@@ -7,6 +7,6 @@ public class Blackjack {
     static public int valueOf(int value, Character card) {
 		if (card=='A') return(value+11<=21)?11:1;
 		if (card=='J' || card=='Q' || card=='K') return 10; 
-		else return (int)card - '0'; //Card 10 would be 1. There is no conversion from char "1" into 10. Either better to store as a string or convert using a dictionary.
+		else return (int)card - '0'; //Card 10 would be 1. There is no conversion from char "1" into 10. Either better to store as a string or convert using a dictionary (Map).
     }
 }
