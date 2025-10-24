9e7ab4d261de022566d2eb3e63427539de56e83d
Pavel Dohnal <74378788+pashadohnal@users.noreply.github.com>
2025-10-24 10:49:59 +0800

    Update Blackjack.java

--- src/blackjack/Blackjack.java
+++ src/blackjack/Blackjack.java
@@ -6,7 +6,7 @@ public class Blackjack {
     // it is better to let the player class to calculate the value
     static public int valueOf(int value, Character card) {
 		if (card=='A') return(value+11<=21)?11:1;
-		if (card=='J' || card=='Q' || card=='K') return 10;
-		else return (int)card - '0';
+		if (card=='J' || card=='Q' || card=='K') return 10; 
+		else return (int)card - '0'; //Card 10 would be 1. There is no conversion from char "1" into 10. Either better to store as a string or convert using a dictionary.
     }
 }
