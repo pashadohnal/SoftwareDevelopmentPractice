6dd326e1c8f54cde6c8bcee3edd616c564ad62bd
Pavel Dohnal <pavd@itu.dk>
2025-11-19 18:25:14 +0800

    Revert "Merge remote-tracking branch 'origin/Refactoring' into Refactoring"
    This reverts commit e7bd28aae11cd1f7899be94a267501d1d9b827fc, reversing
    changes made to 25dc4f445bb3e2909cb9765cc05d0449281872b1.

--- src/main/gameModes/Local.java
+++ src/main/gameModes/Local.java
@@ -53,16 +53,11 @@ public class Local {
 	        boolean playMoreRounds = true;
 	        while (playMoreRounds) {
 	            reset(decks, dealer, human, bots);
-<<<<<<< HEAD
 	            initBet(scanner, human);
 	            if (human.getBalance() <= 0) {
-=======
-				if (human.getBalance() <= 0) {
->>>>>>> refs/remotes/origin/Refactoring
 	                System.out.println("You have no money left for another round.");
 	                break;
 	            }
-	            initBet(scanner, human, bots);
 	            initPlayers(dealer, human, bots);
 	            sideBet(scanner, dealer);
 	            playerTurn(scanner, human, "Player");
